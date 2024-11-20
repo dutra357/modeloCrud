d1c451ad1e6fbda21cba690c4813fe61eaa13b72
David Dutra <144396143+dutra357@users.noreply.github.com>
2024-11-20 15:55:05 -0300

    Início do projeto.
    Upload do spring initializer.

---
+++ src/main/java/com/dutra/modeloCrud/ModeloCrudApplication.java
@@ -0,0 +1,13 @@
+package com.dutra.modeloCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dutra/modeloCrud/ModeloCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dutra.modeloCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
