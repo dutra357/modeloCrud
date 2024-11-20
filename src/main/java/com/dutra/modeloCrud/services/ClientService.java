45b78931568e1b84b9ff8cafbbe59894e1dfadf4
David Alves Dutra <dadutra@hotmail.com>
2024-11-20 16:57:59 -0300

    Adicionada interface para service. Iniciado tratamento dos mṕetodos.

--- src/main/java/com/dutra/modeloCrud/services/ClientService.java
+++ src/main/java/com/dutra/modeloCrud/services/ClientService.java
@@ -1,7 +1,43 @@
 package com.dutra.modeloCrud.services;
 
+import com.dutra.modeloCrud.dtos.ClientEntry;
+import com.dutra.modeloCrud.dtos.ClientResponse;
+import com.dutra.modeloCrud.repositories.ClientRepository;
+import com.dutra.modeloCrud.services.interfaces.ClientServiceInterface;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ClientService {
+public class ClientService implements ClientServiceInterface {
+
+    private final ClientRepository repository;
+    public ClientService(ClientRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public ClientResponse findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Page<ClientResponse> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public ClientResponse saveClient(ClientEntry newClient) {
+        return null;
+    }
+
+    @Override
+    public ClientResponse updateClient(ClientEntry clientUpdated) {
+        return null;
+    }
+
+    @Override
+    public void deleteClient(Long id) {
+
+    }
 }
--- src/main/java/com/dutra/modeloCrud/services/interfaces/ClientServiceInterface.java
+++ src/main/java/com/dutra/modeloCrud/services/interfaces/ClientServiceInterface.java
@@ -1,6 +1,15 @@
 package com.dutra.modeloCrud.services.interfaces;
 
+import com.dutra.modeloCrud.dtos.ClientEntry;
+import com.dutra.modeloCrud.dtos.ClientResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ClientServiceInterface {
 
-
+    ClientResponse findById(Long id);
+    Page<ClientResponse> findAll(Pageable pageable);
+    ClientResponse saveClient(ClientEntry newClient);
+    ClientResponse updateClient(ClientEntry clientUpdated);
+    void deleteClient(Long id);
 }
